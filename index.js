799d404185be5ab28bf61e40dd83634a692895c3
Miguh Ruiz <miguhruiz@gmail.com>
2017-09-16 17:24:35 +0200

    [Update] Declaration of routes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const apiRoutes = require('./routes/api')
 
 const app = express()
 const port = process.env.PORT || '3000'
@@ -6,6 +7,12 @@ const port = process.env.PORT || '3000'
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
+
+app.get('/api/posts/', apiRoutes.loadPosts)
+app.get('/api/posts/:id', apiRoutes.loadPost)
+app.post('/api/posts/', apiRoutes.newPost)
+app.put('/api/posts/:id', apiRoutes.updatePost)
+app.delete('/api/posts/:id', apiRoutes.deletePost)
 
 app.listen(port, () => {
     console.log(`[Express App] The app is listening on port: ${port}`)
